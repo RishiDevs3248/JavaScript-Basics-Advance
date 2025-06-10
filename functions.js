73aa996fe65973d2581eea44ace2bb1e68ca0fe0
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-06-11 01:48:07 +0530

    Functions

--- functions.js
+++ functions.js
@@ -21,13 +21,13 @@ let sum = function (a,b){
 sum(4,5)
 
 // method 2 = arrow function
-let sum2 =(a,b)=>{
+let sum2 = (a,b)=>{
     return a+b;
 }
 sum2(4,6)
 
 
-// if you only have a single parameter you don't need to give "()" brackets , and if you only have a return fun in your body you dont give "{}" brackets and don't need to write the return keywors
+// if you only have a single parameter you don't need to give "()" brackets , and if you only have a return fun in your body you dont give "{}" brackets and don't need to write the return keyword
 
-let example = a=> a+10;
+let example = a => a+10;
 
