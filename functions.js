126b7a1a0e071f7717765d75ef3b8108c23bd689
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-06-10 10:35:48 +0530

    function types

---
+++ functions.js
@@ -0,0 +1,33 @@
+// Traditional functions 
+
+// function function_name(parameters){
+//      body
+// }
+
+// function_name(arguments)
+
+function sum3(a,b){
+    return a+b;
+}
+
+
+
+// function expression
+
+// method 1 = anonymous function
+let sum = function (a,b){
+    return a+b;
+}
+sum(4,5)
+
+// method 2 = arrow function
+let sum2 =(a,b)=>{
+    return a+b;
+}
+sum2(4,6)
+
+
+// if you only have a single parameter you don't need to give "()" brackets , and if you only have a return fun in your body you dont give "{}" brackets and don't need to write the return keywors
+
+let example = a=> a+10;
+
