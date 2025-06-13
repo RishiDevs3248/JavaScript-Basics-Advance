3e3a1fb681cbe3c11a345890cd2053b4147543f2
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-06-13 10:35:41 +0530

    array Questions

---
+++ 3_Arrays.js
@@ -0,0 +1,36 @@
+let arr = [1,2,3,4,5,6];
+
+// array basics 
+
+
+
+// ------------------------------------------------------------------------------------------------------
+//problem - 9 : filter no grater than 5
+// arr.filter((ele)=>{
+//     return ele>5;
+// })
+
+
+// ------------------------------------------------------------------------------------------------------
+//problem - 10 : double each elements in array
+// let arr2 =[]
+// for(let i=0; i<arr.length ; i++){
+//     arr2.push(arr[i]*2);
+// }
+// console.log(arr2)
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem - 11 : print all ele using for each
+// arr.forEach((ele)=>{
+//     console.log(ele)
+// })
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 12 - double each element using map
+// let newArr = arr.map((ele)=>{
+//     return ele*2
+// })
+// console.log(newArr)
+
