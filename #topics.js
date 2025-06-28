4826a30f3562e33595287c29ff9d0f3aaa33cc10
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-06-28 17:57:51 +0530

    index added

---
+++ #topics.js
@@ -0,0 +1,45 @@
+// ------------------------------------------------------------------------------------------------------
+
+// 2_functions.js
+// 2.1   - Traditional functions 
+// 2.2   - function expression (has two method)
+// 2.2.1 - method 1 = anonymous function
+// 2.2.2 - method 2 = arrow function
+
+// ------------------------------------------------------------------------------------------------------
+
+// 3_Arrays.js
+// 3.1   - Array operations
+// 3.1.1 - inserting at the end
+// 3.1.2 - inserting at the start
+// 3.1.3 - inserting in middle
+// 3.1.4 - removing from end
+// 3.1.5 - removing from start
+// 3.1.6 - removing from middle
+// 3.2   - finding index of an element
+// 3.3   - reversing an array
+// 3.4   - sorting an array
+// 3.5   - Concatination an array
+// 3.6   - higher order functions
+// 3.6.1 - forEach
+// 3.6.1 - Map
+// 3.6.1 - Fliter
+
+// ------------------------------------------------------------------------------------------------------
+
+// 4_2D_Arrays.js
+// 4.1   - Accessing elements in different ways
+
+// ------------------------------------------------------------------------------------------------------
+
+// 5_Objects.js
+// 5.1   - ways to create an objects
+// 5.2   - accessing a object
+// 5.3   - to get all the keys, values, & key:value
+// 5.4   - destructuring 
+// 5.4.1 - method 1 - Array destructuring 
+// 5.4.2 - method 2 - Object destructuring 
+// 5.5   - spread operator (...)
+// 5.6   - rest operator (...)
+// 5.7   - map : Basic idea of map
+// 5.8   - JSON : Basic idea of json
