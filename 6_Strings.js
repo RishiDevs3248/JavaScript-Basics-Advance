6e19e6a6e942713179c78f4233b3a05244ec50f9
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-06-30 12:15:04 +0530

    Strings

--- 6_Strings.js
+++ 6_Strings.js
@@ -38,5 +38,102 @@ let x = "a.b..c..d."
 // console.log(split("a.b..c..d..","."))
 
 
+//------------------------------------------------------------------------------------------------------
+// Strings from basics
 
-console.log(x.split(".").reverse().join(""))
+// let str = "rishi";
+
+// console.log(str[0])
+// console.log(str.charAt(0))
+
+// console.log(str.length)
+
+// for(let i=0 ; i<str.length ; i++){
+//     console.log(str[i])
+// }
+
+// for(let ch of str){
+//     console.log(ch)
+// }
+
+
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+
+
+// console.log(str.indexOf("i"))
+// console.log(str.lastIndexOf("i"))
+
+// console.log(str.replace("i","I")) // just replace first found 
+// console.log(str.replaceAll("i","I"))
+
+
+// console.log(str.slice(0,3))
+
+
+// console.log(str.charCodeAt(0))
+
+
+// console.log(str.includes("i"))
+// console.log(str.startsWith("r"))
+// console.log(str.endsWith("i"))
+
+
+// let trimstr = "       eldjcnm.      "
+// console.log(trimstr)
+// console.log(trimstr.trim())
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 1 - count vowels
+
+// let str = "qwertyuiopasdfghjklxcvbnm";
+// str = str.toLowerCase()
+
+// let count = 0
+// for(let i=0 ; i < str.length ; i++){
+//     if(str[i] == "a" || str[i] == "e" ||str[i] == "i" ||str[i] == "o" ||str[i] == "u" ){
+//         count++
+//     }
+// }
+
+// console.log(count)
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 2 - reverse s string
+
+// console.log(str.split("").reverse().join(""))
+
+// let rev = ""
+// for(let i=str.length-1 ; i>=0 ; i-- ){
+//     rev += str[i]
+// }
+// console.log(rev)
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 3 - is palindrome
+
+// let str = "coooooooooooooc";
+// if(str == str.split("").reverse().join("")){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 4 - replace all space with "_"
+
+// let str = "a sd fcmk lkweldw mcewlk cwqlk"
+// console.log(str.replaceAll(" ","_"))
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 4 - count words in a sentence
+
+// let str = "a sd fcmk lkweldw mcewlk cwqlk"
+// console.log(str.split(" ").length)
+
