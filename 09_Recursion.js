5a784de24c39a2c403861a78389ee12928ac28a3
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-07-11 12:23:34 +0530

    Recursion

---
+++ 09_Recursion.js
@@ -0,0 +1,105 @@
+// def - Recursion is a programming technique where a function calls itself until the base case is hit to solve a problem.
+
+// function function_name(parameters){
+//     // step 1 - base case
+
+
+//     // step 2 - kaan (your work)
+
+
+//     // step 3 - recursion
+// }
+
+
+
+// Note -
+// when some code is written before recursive function call it gets executed going upward in the call stack
+// when some code is written after recursive function call it gets executed going downward in the call stack
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 1 -  print in decreasing no.
+// function decREc(n){
+//     if(n  == 0){
+//         return;
+//     }
+//     console.log(n)
+//     decREc(n-1)
+// }
+// decREc(5)
+
+
+
+        
+// ------------------------------------------------------------------------------------------------------
+// problem 2 - print in incresing no.
+// function incRec(n){
+//     if(n == 0){
+//         return;
+//     }
+//     incRec(n-1)
+//     console.log(n)
+// }
+// incRec(5)    
+        
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 3 - print in decreasing and incresing no.
+// function Rec(n){
+//     if(n == 0){
+//         return;
+//     }
+//     console.log(n)
+//     Rec(n-1)
+//     console.log(n)
+// }
+// Rec(5) 
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 4 - factorial
+// function fact(n){
+//     if(n<1){
+//         return 1;
+//     }
+//     return n*fact(n-1)
+// }
+// console.log(fact(5))
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 5 - x raised to n (power)
+// function pow(x,n){
+//     if(n<=1){
+//         return x;
+//     }
+//     return x*pow(x,n-1)
+// }
+// console.log(pow(5,4))
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------
+// problem 6 - 
+// function pow(x,n){
+//     if(n<=1){
+//         return x;
+//     }
+//     return x*pow(x,n-1)
+// }
+// console.log(pow(5,4))
